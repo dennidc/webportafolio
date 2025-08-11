ba6ea2864bc82e4cc2637b6ede5b8ef247f3557f
Denni <146395573+dennidc@users.noreply.github.com>
2025-08-11 17:55:01 -0600

    cambios en el nav, agregando botón que redirige hasta arriba

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,40 @@
 const nav = document.querySelector('#nav');
 const abrir = document.querySelector('#iconomenu');
 const cerrar = document.querySelector('#CerrarMenu');
-
+const enlaces = document.querySelectorAll('#nav a');
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+// Abrir menú
 abrir.addEventListener('click', () => {
   nav.classList.add('activo');
-})
+});
 
+// Cerrar menú con el botón de cerrar
 cerrar.addEventListener('click', () => {
   nav.classList.remove('activo');
-})
+});
+
+// Cerrar menú al dar clic en un enlace
+enlaces.forEach(enlace => {
+  enlace.addEventListener('click', () => {
+    nav.classList.remove('activo');
+  });
+});
+
+// Cerrar menú al dar clic fuera
+document.addEventListener('click', (event) => {
+  if (
+    nav.classList.contains('activo') &&
+    !nav.contains(event.target) &&
+    event.target !== abrir
+  ) {
+    nav.classList.remove('activo');
+  }
+});
+
+// Efecto scroll suave al hacer clic
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Hace el desplazamiento suave
+  });
+});
