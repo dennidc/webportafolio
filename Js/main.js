909f6d8473319edd5bc7a806354adadec960b87d
Denni <146395573+dennidc@users.noreply.github.com>
2025-08-11 12:31:16 -0600

    agregado funcionalidad del nav en versión móvil(falta mejorar)

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,11 @@
-  const iconoMenu = document.getElementById("iconomenu");
-  const menu = document.querySelector(".menu");
+const nav = document.querySelector('#nav');
+const abrir = document.querySelector('#iconomenu');
+const cerrar = document.querySelector('#CerrarMenu');
 
-  iconoMenu.addEventListener("click", () => {
-    menu.classList.toggle("activo");
-  });
+abrir.addEventListener('click', () => {
+  nav.classList.add('activo');
+})
+
+cerrar.addEventListener('click', () => {
+  nav.classList.remove('activo');
+})
